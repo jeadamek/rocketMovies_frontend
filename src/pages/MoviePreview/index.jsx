b66298332dfce2bce50b2965aa8332b3f33838bd
jessica <jessica.g.adamek@gmail.com>
2023-03-09 17:18:11 -0300

    build: add global style

--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -3,7 +3,8 @@ import { Container } from "./styles";
 export function MoviePreview() {
   return(
     <Container>
-
+      <h1>Hello</h1>
+      <p>hello world</p>
     </Container>
   )
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -6,9 +6,58 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  :root {
+    font-size: 62.5%;
+  }
   
   body {
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Roboto Slab', serif;
+    font-size: 1.6rem;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+
+  /* SCROLLBAR STYLE */
+  /* Firefox */
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: ${({ theme }) => theme.COLORS.PINK} ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  }
+
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: .6rem;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 1rem;
+    border: none;
   }
 
 `;
