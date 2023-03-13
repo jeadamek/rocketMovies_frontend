fad3d51e56f9280bd003bad91edff60a53bcb087
jessica <jessica.g.adamek@gmail.com>
2023-03-12 22:07:23 -0300

    build: add sign in page

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,39 @@
+import { Container, Form, Background } from './styles';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import { FiMail, FiLock } from 'react-icons/fi';
+
+export function SignIn() {
+  return(
+    <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input 
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+        />
+        
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar" type="submit" />
+
+        <a href="#">
+          Criar Conta
+        </a>
+      </Form>
+
+      <Background />
+    </Container>
+  );
+}
