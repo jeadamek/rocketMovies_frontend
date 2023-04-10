24a02d9c70ef8bb9639ce00732948d6e7c6dee8a
jessica <jessica.g.adamek@gmail.com>
2023-04-10 18:47:20 -0300

    :construction: create context for signIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,24 @@
-import { Container, Form, Background } from './styles';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
-import { Link } from 'react-router-dom';
-import { FiMail, FiLock } from 'react-icons/fi';
+import { Container, Form, Background } from './styles';
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return(
     <Container>
       <Form>
@@ -19,15 +31,20 @@ export function SignIn() {
           placeholder="E-mail"
           type="email"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
         
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" type="submit" />
+        <Button 
+          title="Entrar"
+          onClick={handleSignIn}
+        />
 
         <Link to="/register">
           Criar Conta
