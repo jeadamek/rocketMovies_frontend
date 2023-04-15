cb8ce683a6b03f04b9332dc72bbec634d468ed9e
jessica <jessica.g.adamek@gmail.com>
2023-04-15 17:28:56 -0300

    :construction: add new movie: add and delete tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { Container, Form, Section } from './styles';
 
 import { Input } from '../../components/Input';
@@ -10,6 +13,31 @@ import { MovieItem } from '../../components/MovieItem';
 import { FiArrowLeft } from "react-icons/fi";
 
 export function New() {
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(null);
+  const [description, setDescription] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleAddTag(){
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted))
+  }
+
+  function handleDeleteMovie(){
+    const confirm = window.confirm("Deseja realmente deletar o filme?");
+    if (confirm) {
+      navigate(-1)
+    }
+  }
+
   return(
     <Container>
       <Header />
@@ -36,8 +64,23 @@ export function New() {
           <Section>
             <h2>Marcadores</h2>
             <div className="tags">
-              <MovieItem value="ação"/>
-              <MovieItem isNew placeholder="Novo Marcador"/>
+              {
+                tags.map((tag, index) => (
+                  <MovieItem 
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)} 
+                  />
+                ))
+              }
+
+              <MovieItem 
+                isNew 
+                placeholder="Novo Marcador"
+                value={newTag}
+                onChange={e => setNewTag(e.target.value)} 
+                onClick={handleAddTag} 
+              />
             </div>
           </Section>
 
@@ -45,6 +88,7 @@ export function New() {
             <Button 
               title="Excluir filme" 
               className="secondary"
+              onClick={handleDeleteMovie}
             />
             
             <Button 
